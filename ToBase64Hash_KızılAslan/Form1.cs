92143f5559d2b5d7b8f391d7ca3cc9fd72d4379c
kizil-aslan <71307248+kizil-aslan@users.noreply.github.com>
2021-09-18 18:26:05 +0430

    First

---
+++ ToBase64Hash_KızılAslan/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToBase64Hash_KızılAslan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] veridizim = ASCIIEncoding.ASCII.GetBytes(rchtxtMetin.Text);
+                rchtxtSifre.Text = Convert.ToBase64String(veridizim);
+            }
+            catch (Exception Aciklama)
+            {
+                MessageBox.Show(Aciklama.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] cozulenvarilerim = Convert.FromBase64String(rchtxtSifre.Text);
+                rchtxtMetin.Text = ASCIIEncoding.ASCII.GetString(cozulenvarilerim);
+            }
+            catch (Exception Aciklama)
+            {
+                MessageBox.Show(Aciklama.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
